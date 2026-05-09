1ab2540ff39ed84362e8783acef5c87c5e8b810c
SARTHAK MANE <sarthakmane475@gmail.com>
2026-05-09 10:36:50 +0530

    Add files via upload

---
+++ add_two_integer.py
@@ -0,0 +1,14 @@
+
+#2235. Add Two Integers
+
+
+
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1 + num2
+        
