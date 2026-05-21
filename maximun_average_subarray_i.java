2f39e98583416e1a2d618f2127304ede42ddf895
SARTHAK MANE <sarthakmane475@gmail.com>
2026-05-21 22:13:20 +0530

    Add files via upload

---
+++ maximun_average_subarray_i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double ans=0;
+        double sum=0;
+        for(int i=0;i<k;i++) {
+            sum+=nums[i];
+        }
+        ans=sum/k;
+        for(int i=k;i<nums.length;i++) {
+            sum+=nums[i];
+            sum-=nums[i-k];
+            ans=Math.max(ans, sum/k);
+        }
+        return ans;
+    }
+}
