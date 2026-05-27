4768a1e4f9c834c363689822d84cacb79efc9d18
SARTHAK MANE <sarthakmane475@gmail.com>
2026-05-27 22:20:58 +0530

    Add files via upload

---
+++ unique_number_of_occurrences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(int n:arr){
+            map.put(n, map.getOrDefault(n,0)+1);
+        }
+        HashSet<Integer> set =new HashSet<Integer>();
+        for(int freq:map.values()){
+            if(!set.add(freq)){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
