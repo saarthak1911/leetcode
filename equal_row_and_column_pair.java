16e7260c5180c54955e245954f3488565a46c00e
SARTHAK MANE <sarthakmane475@gmail.com>
2026-05-29 23:44:44 +0530

    Add files via upload

---
+++ equal_row_and_column_pair.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int equalPairs(int[][] grid) {
+        int n = grid.length;
+        int pairs = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        // 📝 Store rows
+        for (int i = 0; i < n; i++) {
+            String row = Arrays.toString(grid[i]);
+            map.put(row, map.getOrDefault(row, 0) + 1);
+        }
+        
+        // 🔎 Check columns
+        for (int j = 0; j < n; j++) {
+            int[] col = new int[n];
+            for (int i = 0; i < n; i++) {
+                col[i] = grid[i][j];
+            }
+            String colVal = Arrays.toString(col);
+            if (map.containsKey(colVal)) {
+                pairs += map.get(colVal);
+            }
+        }
+        
+        return pairs;
+    }
+}
