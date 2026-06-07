8d6e9b44655647bb53e4d75a355c47cabd776216
SARTHAK MANE <sarthakmane475@gmail.com>
2026-06-07 21:34:01 +0530

    Add files via upload

---
+++ max_twin_sum_of_linked_list.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        int ans = 0;
+        ListNode newList = null;
+        ListNode current = head;
+        ListNode currHalf = head;
+        
+        while (currHalf != null && currHalf.next != null) {
+            currHalf = currHalf.next.next;
+            ListNode temp = current.next;
+            current.next = newList;
+            newList = current;
+            current = temp;
+        }
+
+        while (current != null) {
+            ans = Math.max(ans, current.val + newList.val);
+            current = current.next;
+            newList = newList.next;
+        }
+        
+        return ans;
+    }
+}
